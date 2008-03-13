0f40088a2fe3a4be50d83408e282bfbe418d76c2
tom.soderlund@differentgame.org <tom.soderlund@differentgame.org@24fb1114-e73d-0410-9022-b5a75242ab44>
2008-03-13 21:57:29 +0000

    New simple TurntableControl (buggy, no input code defined). New graphics.

--- trunk/iDJ-iPhoneSDK/Classes/FaderControl.m
+++ trunk/iDJ-iPhoneSDK/Classes/FaderControl.m
@@ -31,10 +31,20 @@ CALayer *_buttonLayer;
 		_buttonLayer.position = CGPointMake(frame.size.width/2 - 2, frame.size.height/2);
 		UIImage *buttonImage = [UIImage imageNamed:@"fader_button.png"];
 		_buttonLayer.contents = (id)buttonImage.CGImage;
+		//[_buttonLayer setValue:[NSNumber numberWithInt: 45] forKeyPath:@"transform.rotation"];
 		[self.layer addSublayer:_buttonLayer];
     }
     return self;
 }
+
+
+- (void)dealloc
+{
+	[super dealloc];
+	[_backgroundLayer release];
+	[_buttonLayer release];
+}
+
 
 /** touchesChangedWithEvent: multi-touch handling. */
 - (void)touchesChangedWithEvent:(UIEvent*)event
@@ -52,23 +62,39 @@ CALayer *_buttonLayer;
 		if (newX > _backgroundLayer.bounds.size.width - _buttonLayer.bounds.size.width/2)
 			newX = _backgroundLayer.bounds.size.width - _buttonLayer.bounds.size.width/2;
 		_buttonLayer.position = CGPointMake(newX, _buttonLayer.position.y);
+		NSLog(@"touchesChangedWithEvent: value %.3f", [self getCurrentValue]);
+		
+		//[self.nextResponder touchesBegan:touches forEvent:event];
     }
+}
+
+/** getCurrentValue: returns a value from 0.0 to 1.0. */
+- (float)getCurrentValue 
+{
+	float minValue = _buttonLayer.bounds.size.width/2;
+	float maxValue = _backgroundLayer.bounds.size.width - _buttonLayer.bounds.size.width/2;
+	return (_buttonLayer.position.x - minValue) / (maxValue - minValue);
+}
+
+- (void)setCurrentValue:(float)value
+{
+	int newX = _buttonLayer.bounds.size.width/2 + value * (_backgroundLayer.bounds.size.width - _buttonLayer.bounds.size.width);
+	if (newX < _buttonLayer.bounds.size.width/2)
+		newX = _buttonLayer.bounds.size.width/2;
+	if (newX > _backgroundLayer.bounds.size.width - _buttonLayer.bounds.size.width/2)
+		newX = _backgroundLayer.bounds.size.width - _buttonLayer.bounds.size.width/2;
+	_buttonLayer.position = CGPointMake(newX, _buttonLayer.position.y);
 }
 
 /** drawRect: paint the FaderControl on the screen. */
 /*
 - (void)drawRect:(CGRect)rect {
-	CGContextRef		context = UIGraphicsGetCurrentContext();
-	CGRect				bounds = [self bounds];
-
+	// Paint rectangle
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGRect bounds = [self bounds];
     CGContextSetRGBFillColor(context, 0.5, 0.5, 0.5, 1.0);
 	CGContextFillRect(context, bounds);
 }
 */
-
-- (void)dealloc
-{
-	[super dealloc];
-}
 
 @end
