e9090b301e6fc874e569c52233167a495a11d98c
tom.soderlund@differentgame.org <tom.soderlund@differentgame.org@24fb1114-e73d-0410-9022-b5a75242ab44>
2008-04-10 15:00:19 +0000

    Toms version of Turntable etc.

---
+++ iDJ-iPhoneSDK/Classes/TurntableControl.h
@@ -0,0 +1,18 @@
+//
+//  TurntableControl.h
+//  iDJ-iPhoneSDK
+//
+//  Created by Tom Söderlund on 3/13/08.
+//  Copyright 2008 "Team iDJ". All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "iDJUtility.h"
+
+@interface TurntableControl : UIView {
+}
+
+-(void) onTimerEvent:(NSTimer *)timer;
+
+@end
